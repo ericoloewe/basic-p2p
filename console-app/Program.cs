a8e6f6db29a88c43d4c849a196b86a05df5b0e83
Érico de Souza Loewe <erico.loewe@cwi.com.br>
2019-11-12 22:25:43 -0300

    feat: add basic implementation

---
+++ console-app/Program.cs
@@ -0,0 +1,115 @@
+﻿using peer;
+using System;
+using System.IO;
+
+namespace console_app
+{
+    class Program
+    {
+        private static Peer peer = new Peer();
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                ProcessCommand(args);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.WriteLine("There was a problem to process command");
+            }
+        }
+
+        private static void ProcessCommand(string[] args)
+        {
+            var command = args[0];
+
+            if (string.IsNullOrEmpty(command))
+            {
+                throw new ArgumentException($"Invalid command => {command}");
+            }
+
+            var parsedCommand = command.Trim().ToLower();
+
+            switch (parsedCommand)
+            {
+                case "connect":
+                    {
+                        Connect(args[1], args[2]);
+                        break;
+                    }
+                case "list":
+                    {
+                        ListFiles();
+                        break;
+                    }
+                case "upload":
+                    {
+                        UploadToNodes(args[1]);
+                        break;
+                    }
+                case "download":
+                    {
+                        DownloadFile(args[1], args[2]);
+                        break;
+                    }
+                default:
+                    {
+                        throw new ArgumentException($"Invalid command => {command}");
+                    }
+            }
+        }
+
+        private static void Connect(string nodeHost, string nodeIp)
+        {
+            if (string.IsNullOrEmpty(nodeHost) || string.IsNullOrEmpty(nodeIp))
+            {
+                throw new ArgumentException($"Invalid host or ip");
+            }
+
+            peer.Connect(nodeHost, nodeIp);
+            Console.WriteLine($"Connected to {nodeHost} {nodeIp}");
+        }
+
+        private static void ListFiles()
+        {
+            Console.WriteLine("List of files: ");
+
+            foreach (var file in peer.Files)
+            {
+                Console.WriteLine($"- {file.Name}");
+            }
+        }
+
+        private static void DownloadFile(string fileName, string downloadPath)
+        {
+            if (string.IsNullOrEmpty(fileName) || string.IsNullOrEmpty(downloadPath))
+            {
+                throw new ArgumentException($"Invalid fileName or downloadPath");
+            }
+
+            var fileBytes = peer.DownloadFile(fileName);
+
+            using (var fs = new FileStream(downloadPath, FileMode.Create, FileAccess.Write))
+            {
+                fs.Write(fileBytes, 0, fileBytes.Length);
+            }
+
+            Console.WriteLine($"The file {fileName} was downloaded to {downloadPath}");
+        }
+
+        private static void UploadToNodes(string relativePath)
+        {
+            if (string.IsNullOrEmpty(relativePath))
+            {
+                throw new ArgumentException($"Invalid path");
+            }
+
+            string filePath = Path.GetFullPath(relativePath);
+
+            peer.UploadFile(filePath);
+            Console.WriteLine($"The file {filePath} was uploaded");
+        }
+    }
+}
---
+++ peer/Peer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace peer
+{
+    public class Peer
+    {
+        public IEnumerable<File> Files { get; set; }
+
+        public void Connect(string nodeHost, string nodeIp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] DownloadFile(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UploadFile(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class File
+    {
+        public string Name { get; set; }
+    }
+}
