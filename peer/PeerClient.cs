5c621d49888454b3d59d00d024fbc0ac26093338
Erico de Souza Loewe <ed-lowe@hotmail.com>
2019-11-23 14:50:49 -0300

    feat(peer): add client and info

---
+++ peer/PeerClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace peer
+{
+    public class PeerClient : IDisposable
+    {
+        public void Connect(string nodeIp, int nodePort)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] DownloadFile(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UploadFile(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ peer/PeerInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace peer
+{
+    public class PeerInfo
+    {
+        public int Id { get; }
+
+        public PeerInfo(int id)
+        {
+            Id = id;
+        }
+
+        internal static PeerInfo FromString(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
