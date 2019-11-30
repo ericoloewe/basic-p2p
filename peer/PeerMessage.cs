ac635bfae94348fa8b06bdef93e804fa085eaed0
Erico de Souza Loewe <ed-lowe@hotmail.com>
2019-11-30 16:17:01 -0300

    feat(peer): add message

--- peer/PeerMessage.cs
+++ peer/PeerMessage.cs
@@ -1,26 +1,86 @@
-﻿namespace peer
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace peer
 {
-    internal class PeerMessage
+    public class PeerMessage
     {
+        public static readonly byte[] END_DELIMITER = Encoding.UTF8.GetBytes(@"\end");
+        private static readonly byte[] END_HEAD_DELIMITER = Encoding.UTF8.GetBytes(@"\endhead");
         public bool HasFile { get { return File != null; } }
-        public bool HasMessage { get { return message != null; } }
         public PeerFile File { get; }
+        public PeerCommandType Type { get; set; }
 
-        private string message;
+        private byte[] head;
+        private byte[] body;
 
-        public PeerMessage(string message)
+        public PeerMessage(byte[] bytes)
         {
-            this.message = message;
+            var bytesAsList = bytes.ToList();
+            Type = (PeerCommandType)bytes[0];
+
+            var endHeadIndex = FindBytesIndex(bytes, END_HEAD_DELIMITER);
+            var endIndex = FindBytesIndex(bytes, END_DELIMITER);
+            int startBodyIndex = endHeadIndex + END_HEAD_DELIMITER.Length;
+
+            head = bytesAsList.GetRange(1, endHeadIndex).ToArray();
+            body = bytesAsList.GetRange(startBodyIndex, endIndex).ToArray();
         }
 
         public PeerMessage(PeerFile file)
         {
-            this.File = file;
+            var fileHead = Encoding.UTF8.GetBytes(file.ToString());
+
+            Type = PeerCommandType.FILE;
+            head = fileHead.Concat(END_HEAD_DELIMITER).ToArray();
+            body = file.Slice;
         }
 
-        public override string ToString()
+        public PeerMessage(PeerCommandType Type)
         {
-            return message;
+            this.Type = Type;
+            head = END_HEAD_DELIMITER;
+            body = new byte[1024];
         }
+
+        public byte[] ToBytes()
+        {
+            return new byte[] { (byte)Type }
+            .Concat(head)
+            .Concat(END_HEAD_DELIMITER)
+            .Concat(body)
+            .Concat(END_DELIMITER).ToArray();
+        }
+
+        private int FindBytesIndex(byte[] haystack, byte[] needle)
+        {
+            // iterate backwards, stop if the rest of the array is shorter than needle (i >= needle.Length)
+            for (var i = haystack.Length - 1; i >= needle.Length - 1; i--)
+            {
+                var found = true;
+                // also iterate backwards through needle, stop if elements do not match (!found)
+                for (var j = needle.Length - 1; j >= 0 && found; j--)
+                {
+                    // compare needle's element with corresponding element of haystack
+                    found = haystack[i - (needle.Length - 1 - j)] == needle[j];
+                }
+
+                if (found)
+                    // result was found, i is now the index of the last found element, so subtract needle's length - 1
+                    return i - (needle.Length - 1);
+            }
+            // not found, return -1
+            return -1;
+        }
+    }
+
+    public enum PeerCommandType
+    {
+        CONNECTIONS,
+        EXIT,
+        FILE,
+        STOP,
+        GENERIC_ERROR,
     }
 }
